29e095b9d72b5fe2dabbb3e64d2d5ac496b7ff6f
shakkiraafsal <102407349+shakkiraafsal@users.noreply.github.com>
2023-02-09 12:24:44 +0530

    bugs correction

--- resources/views/section/Document/approval-pending.blade.php
+++ resources/views/section/Document/approval-pending.blade.php
@@ -5,7 +5,7 @@
    
          <div class="pagetitle">
       <div class="alert alert-primary" style="background-color:#1E2F97;color: white;text-transform: uppercase;">
-        <a href="{{route('section.document-list')}}" class="btn  btn-primary" ><i class="bi bi-plus-circle-fill"></i> Document List</a> 
+        <a href="{{route('section.createdocument')}}" class="btn  btn-primary" ><i class="bi bi-plus-circle-fill"></i> Upload Document</a> 
        </div>
     </div>
     <section class="section">
@@ -17,115 +17,64 @@
             <div class="card-body">
             
   
-  <div class="col-md-3" style="margin-left:auto;" >
-     <h1><input type="text" id="Search" class="form-control" onkeyup="myFunction()" placeholder="Search" title="Type in a name"></h1>
-  </div>
-    <div class="accordion accordion-flush" id="accordionFlushExample">
-          <table class="table table-striped">
-             <tr>
-                        
-                            <th width="25%">Document Name</th>
-                         
-                            <th width="25%">Department</th>
-                             <th width="25%">Documnet Number</th>
-                              <th width="25%">ACTION</th>
-                            
-                        </tr>
-        </table>
-            @if($document->isNotEmpty())
-                
-                @foreach($document as $documents)
-                <div class="accordion-item">
-                  <h2 class="accordion-header" id="flush-headingOne">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-{{$documents->id}}" aria-expanded="false" aria-controls="flush-{{$documents->id}}">
-                    <table class="table table-striped">
-                       
-                        <tr>
-                        
-                        <td width="25%">{{$documents->doc_name}}</td> 
-                     
-                          <td width="25%">{{$documents->fetchDepartmentDetails->dept_name}}</td>
-                            <td width="25%">{{$documents->doc_number}}</td> 
-                                <th width="25%"><button class="btn btn-outline-success">View</button></th>
-                       </tr>
-                    </table>
-                    </button>
-                  </h2>
-                  <div id="flush-{{$documents->id}}" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body">
-                      <div></div>
-                      <iframe src="{{asset($documents->doc_document)}}" width="100%" height="1000" style="border:none;"></iframe>
 
-                   
-                       <table id="example" class="table datatable" style="width:100%">
-                        <tr>
-                          <th>Document Number</th>
-                          <td>{{$documents->doc_number}}</td>
+
+<div class="table-responsive">
+     
+
+       <div class="table-responsive">
+        <table id="example" class="table datatable" style="width:100%">
+                <thead>
+                    <tr>
+                        
+                         <th >Document Number</th>
+                         <th >Document Name</th>
+                         <th >Department</th>
+                         <th >Document Type</th>
+                         <th >Uploaded By</th>
+                         <th >Date</th>
+                         <th>ACTION</th>
+                        
                         </tr>
+                </thead>
+                 <tbody>
+                              @foreach($document as $documents)
                            <tr>
-                          <th>Department</th>
-                          <td>{{$documents->fetchDepartmentDetails->dept_name}}</td>
-                        </tr>
-                           <tr>
-                          <th>Approval Date</th>
-                          <td>{{$documents->doc_approval_date}}</td>
-                        </tr>
-                          <tr>
-                          <th>Uploaded By</th>
-                          <td>{{$documents->doc_uploaded_by}}</td>
-                        </tr>
-                          <tr>
-                          <th>Document Name</th>
-                          <td>{{$documents->doc_name}}</td>
-                        </tr>
-                          <tr>
-                          <th>Remarks</th>
-                          <td>{{$documents->doc_remarks}}</td>
-                        </tr>
-                          <tr>
-                          <th>Created At</th>
-                          <td>{{$documents->created_at}}</td>
-                        </tr>
-                         <tr>
-                          <th>Updated At</th>
-                          <td>{{$documents->updated_at}}</td>
-                        </tr>
-                     
-                            <tr>
-                          <th>Status</th>
-                            <td>
-                                   @if($documents->doc_status == 1)
-                                   <span class="badge bg-success">Approved</span>
-                                   @else
-                                   <span class="badge bg-danger">Pending</span>
-                                  @endif
-                                </td>
-                        </tr>
-                        <tr>
-                         
-                          <td colspan="2">
-                                  
-                      
-                        
-                             <a href="{{asset($documents->doc_document)}}" download class="btn btn-outline-primary" >Download</a> 
-                            <a href="{{asset($documents->doc_document)}}" target="_black" class="btn btn-outline-warning">Preview</a>
+                             
+                              <td>{{$documents->doc_number}}</td>  
+                               <td>{{$documents->doc_name}}</td>
+                              <td>{{$documents->fetchDepartmentDetails->dept_name}}</td>
+                              <td>{{$documents->doc_type}}</td>
+                              <td>{{$documents->doc_uploaded_by}}</td>
+                              <td>{{$documents->created_at}}</td>
+                              <td>  <a href="{{asset($documents->doc_document)}}" target="_black" class="btn btn-outline-warning"><i class="bi bi-eye"></i></a>
+                                 <a href="{{asset($documents->doc_document)}}" download class="btn btn-outline-primary" ><i class="bi bi-box-arrow-down"></i></a> 
+                          </td>
+                              
+                          </tr>
+                            @endforeach
+                            </tbody>
+              </table>
+              </div>
+              <!-- End Table with stripped rows -->
+     
 
-                            
-                        </td>
-                        </tr>
-                    
-                      </table>
-                   
-                    </div>
-                  </div>
-                </div>
-                  @endforeach
+              </div>
 
-              
-                 @else
-                 <div class="alert alert-danger"><i class="bi bi-exclamation-triangle"></i> No documents to list.</div>
-                 @endif
 
+
+
+
+
+
+
+
+
+
+
+
+
+                  
 
               </div>
 
